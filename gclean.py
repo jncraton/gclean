9ea7653a2ce1f6a942299036dee582f8996e4fa5
Jon Craton <jncraton@gmail.com>
2022-10-05 09:40:42 -0400

    Convert to Python 3

--- gclean.py
+++ gclean.py
@@ -22,7 +22,7 @@ def clean_text(text):
   ''
   >>> clean_text(' | | text')
   ' | | text'
-  >>> clean_text(u' \u00a0| \u00a0 |  \\r\\ntest\\r\\n'.encode('utf8')) # Contains non-breaking spaces
+  >>> clean_text(u' \\u00a0| \\u00a0 |  \\r\\ntest\\r\\n'.encode('utf8')) # Contains non-breaking spaces
   '\\r\\ntest\\r\\n'
   >>> clean_text('-=')
   ''
@@ -40,8 +40,8 @@ def clean_text(text):
   text = re.sub('^[\-\=][\-\=\| \t]*', '', text, flags=re.M)
   text = re.sub('^  +', ' ', text, flags=re.M)
 
-  text = re.sub(ur'^[\| \t\u00A0\uC2A0]*\r$', '\r', text, flags=re.M|re.UNICODE)
-  text = re.sub(ur'^[\| \t\u00A0\uC2A0]*$', '', text, flags=re.M|re.UNICODE)
+  text = re.sub(r'^[\| \t\u00A0\uC2A0]*\r$', '\r', text, flags=re.M|re.UNICODE)
+  text = re.sub(r'^[\| \t\u00A0\uC2A0]*$', '', text, flags=re.M|re.UNICODE)
 
   text = re.sub('^ *$', '', text, flags=re.M)
   text = re.sub('\r\n\r\n[\r\n]*', '\r\n\r\n', text)
@@ -66,7 +66,7 @@ if __name__ == '__main__':
 
   def get_labels(message_id):
       result = mail.uid('fetch',message_id,'X-GM-LABELS')
-      print('Getting current labels...%s' % result[0])
+      print(('Getting current labels...%s' % result[0]))
       
       labels_string = result[1][0]
               
@@ -88,7 +88,7 @@ if __name__ == '__main__':
       if label:
           labels.append(label.replace('\\\\', '\\'))
       
-      print 'Current labels ' + str(labels)
+      print('Current labels ' + str(labels))
       
       return labels
   
@@ -106,11 +106,11 @@ if __name__ == '__main__':
       kept = []
       
       # Remove useless headers
-      for header in msg.keys():
+      for header in list(msg.keys()):
           if not header.lower().startswith('x-') and not header.lower() in ['thread-topic','in-reply-to','references','thread-index', 'message-id','content-class','content-language','received','return-path','received-spf','authentication-results','dkim-signature','disposition-notification-to','resent-from','accept-language','domainkey-signature','delivered-to', 'feedback-id', 'list-unsubscribe', 'errors-to', 'precedence', 'bounces-to', 'pp-correlation-id', 'amq-delivery-message-id','content-disposition','content-id','arc-seal','arc-message-signature','arc-authentication-results','authentication-results-original','spamdiagnosticoutput','spamdiagnosticmetadata']:
               kept.append(header)
               headers += header + ': ' + msg[header] + '\r\n'
-      print('Kept headers:%s' % ','.join(kept))
+      print(('Kept headers:%s' % ','.join(kept)))
       
       headers += 'message-id: %08x\r\n' % random.randint(0,4294967296) #nonce to create a "different" message
       
@@ -119,7 +119,7 @@ if __name__ == '__main__':
   mail = imaplib.IMAP4_SSL('imap.gmail.com')
   
   mail.login(config.email, config.password)
-  
+
   def search():
       mail.select("[Google Mail]/All Mail")
       return mail.uid('search', None, 'X-GM-RAW', "(label:_clean OR label:_zero_att) -label:_cleaned -in:inbox")
@@ -127,15 +127,15 @@ if __name__ == '__main__':
   search_result, message_ids = search()
   
   while search_result == 'OK' and message_ids[0]:
-      print str(len(message_ids[0].split(' '))) + ' messages left'
+      print(str(len(message_ids[0].split(' '))) + ' messages left')
       
       for message_id in [message_ids[0].split(' ')[0]]:
           has_plain = False
           
-          print 'Fetching ' + message_id
+          print('Fetching ' + message_id)
   
           flags = mail.uid('fetch', message_id, 'FLAGS')
-          print('Getting current flags...%s' % flags[0])
+          print(('Getting current flags...%s' % flags[0]))
           
           is_read = '\seen' in flags[1][0].lower()
               
@@ -147,7 +147,7 @@ if __name__ == '__main__':
           
           for response_part in msg_data:
               if isinstance(response_part, tuple):
-                  print 'Parsing ' + message_id
+                  print('Parsing ' + message_id)
                   msg = email.message_from_string(response_part[1])
   
                   text_part = None
@@ -175,8 +175,8 @@ if __name__ == '__main__':
                     print("Preparing to write as text/plain message")
 
                     msg.set_payload(clean_text(text_part.get_payload(decode=True)))
-                    for header, value in text_part.items():
-                      print("Merging %s header" % header)
+                    for header, value in list(text_part.items()):
+                      print(("Merging %s header" % header))
                       del msg[header]
                       msg.add_header(header, value)
                     # Force quoted-printable encoding
@@ -188,22 +188,22 @@ if __name__ == '__main__':
                     for part in msg.walk():
                         type = part.get_content_type()
                         if ("html" in type and has_plain):
-                            print 'Removing ' + type + ' from ' + message_id
+                            print('Removing ' + type + ' from ' + message_id)
                             part.set_payload('')
                             if type == 'text/html':
                                 del part['Content-Type']
                                 part['Content-Type'] = 'deleted'
                         elif '_zero_att' in labels and not "text" in type and not part.is_multipart():
-                            print 'Removing attachment ' + type + ' from ' + message_id
+                            print('Removing attachment ' + type + ' from ' + message_id)
                             part.set_payload('')
                         else:
-                            print 'Keeping ' + type + ' from ' + message_id
+                            print('Keeping ' + type + ' from ' + message_id)
           
-          print('Replacing ' + message_id + ' ' + msg['Subject'][:65])          
+          print(('Replacing ' + message_id + ' ' + msg['Subject'][:65]))          
           date = mktime_tz(parsedate_tz(msg['Date']))
           
           result = mail.append('[Google Mail]/All Mail','',date,cleaned_headers(msg))
-          print('Added new message...%s' % result[0])
+          print(('Added new message...%s' % result[0]))
           if result[0] == 'OK':
               print(result)
               match = re.match(r".*APPENDUID \d (\d+)", result[1][0])
@@ -215,24 +215,24 @@ if __name__ == '__main__':
                 labels.append('_converted_to_plain')
               
               if is_read:
-                  print('Marking as read...%s' % mail.uid('store' ,new_id, '+FLAGS', '\\Seen')[0])
+                  print(('Marking as read...%s' % mail.uid('store' ,new_id, '+FLAGS', '\\Seen')[0]))
   
               result = mail.uid('fetch', message_id, '(RFC822)')
-              print('Fetched original message...%s' % result[0])
+              print(('Fetched original message...%s' % result[0]))
               if new_id == message_id:
-                  print 'New message given current UID. Aborting.'
+                  print('New message given current UID. Aborting.')
               elif not result[1][0]:
-                  print 'Appended message replaced current message (no changes?)'
+                  print('Appended message replaced current message (no changes?)')
               else:
                   for label in labels:
                       if not label == '_clean' and not label == '_zero_att':
                           result = mail.uid('store', new_id, '+X-GM-LABELS', label)
-                          print 'Added label ' + label + '...' + result[0]
+                          print('Added label ' + label + '...' + result[0])
                           
                           assert result[0] == 'OK'
   
-                  print('Deleting original message...' + str(mail.uid('store', message_id, '+FLAGS', '\\Deleted')[0]))
-                  print('Expunging mailbox...' + mail.expunge()[0])
+                  print(('Deleting original message...' + str(mail.uid('store', message_id, '+FLAGS', '\\Deleted')[0])))
+                  print(('Expunging mailbox...' + mail.expunge()[0]))
 
       if kill_after:
         exit(0)
